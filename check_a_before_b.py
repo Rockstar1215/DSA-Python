a16ef03236d633dcf13a5cb3d71850f54306a496
Vikram Bhaskar Poojary <116565643+Rockstar1215@users.noreply.github.com>
2025-04-06 21:40:01 +0530

    check A's appear before B's -Leetcode 2124

--- check_a_before_b.py
+++ check_a_before_b.py
@@ -3,7 +3,7 @@ class Solution(object):
         s_len=len(s)
         mylist=[]
         for i  in range(s_len):
-            if s[i]=='a'or s[i]=='b':
+            if s[i]=='a'or s[i]=='b': 
                 mylist.append(s[i])
         for i in range(len(mylist)-1):
             if mylist[i]=='b' and mylist[i+1]=='a':
